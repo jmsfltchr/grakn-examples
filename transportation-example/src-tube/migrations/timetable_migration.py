e43a206890e5fd7cd1534bc0ad1aeda9c55b0c8c
Unknown <james@grakn.ai>
2018-04-25 17:03:06 +0100

    Successfully builds network
    There is a problem, in that routes cannot be uniquely identified by their identifier attribute. Currently I try to do this, which needs changing.

--- transportation-example/src-tube/migrations/timetable_migration.py
+++ transportation-example/src-tube/migrations/timetable_migration.py
@@ -1,92 +1,102 @@
 import json
 import grakn
 import datetime as dt
+import os
 
 
-def import_query_generator():
+def import_query_generator(timetables_dir_path):
     """
     Builds the Grakl statements required to import the transportation data contained in all_routes.json
     :return:
     """
 
-    s = "../../datasets/tfl_api/timetables/940GZZLUEPG_west_ruislip_central_inbound.json"
+    timetable_paths = os.listdir(timetables_dir_path)
+    zone_names = set()
+    stop_ids = set()
+    line_names = set()
+    for timetable_path in timetable_paths:
+        with open(timetables_dir_path + "/" + timetable_path, 'r') as f:
+            data = json.load(f)
+            if data['lineName'] not in line_names:
+                line_names.add(data['lineName'])
+                yield ("insert $line isa line, has name \"{}\";").format(data['lineName'])
 
-    with open(s, 'r') as f:
-        data = json.load(f)
-        yield ("insert $service isa service, has name \"{}\";").format(data['lineName'])
+            for stop in data["stops"]:
+                if stop["id"] not in stop_ids:
+                    # Only proceed if we haven't already seen this stop before
+                    stop_ids.add(stop["id"])
+                    # TODO Check that the stop isn't duplicated, or skip this if instead we only do it once per tube line.
+                    # - Actually there willbe duplication doing it per tube, since stations belong to more than one line
+                    try:
+                        zone_name = stop["zone"]
+                    except KeyError:
+                        # In the case that there is no zone information
+                        zone_name = -1
 
-        zones = set()
-        for stop in data["stops"]:
-            try:
-                zone = stop["zone"]
-            except KeyError:
-                # In the case that there is no zone information
-                zone = -1
+                    if zone_name not in zone_names:
+                        zone_names.add(zone_name)
+                        yield ("insert $zone isa zone, has name \"{}\";".format(zone_name))
 
-            if zone not in zones:
-                zones.add(zone)
-                yield ("insert $zone isa zone, has name \"{}\";".format(zone))
+                    yield ("match\n"
+                           "$zone isa zone, has name \"{}\";\n"
+                           "insert\n"
+                           "$stop isa stop, has naptan-id \"{}\", has name \"{}\", "
+                           "has lat {}, has lon {};\n"
+                           "(contains-stop: $stop, within: $zone) isa zoning;").format(
+                        zone_name,
+                        stop["id"],
+                        stop["name"],
+                        stop["lat"],
+                        stop["lon"]
+                )
 
-            yield ("match\n"
-                   "$zone isa zone, has name \"{}\";\n"
-                   "insert\n"
-                   "$stop isa stop, has naptan-id \"{}\", has name \"{}\", "
-                   "has lat {}, has lon {};\n"
-                   "(contains-stop: $stop, within: $zone) isa zoning;").format(
-                zone,
-                stop["id"],
-                stop["name"],
-                stop["lat"],
-                stop["lon"]
-            )
+            """
+            Get the time between stops
+            """
+            for routes in data['timetable']["routes"]:
 
-        """
-        Get the time between stops
-        """
-        for routes in data['timetable']["routes"]:
+                for station_intervals in routes["stationIntervals"]:
+                    last_naptan_id = data['timetable']["departureStopId"]
+                    last_time_to_arrival = 0
+                    # print("----- id : {} -----".format(station_intervals["id"]))
+                    yield ("match\n"
+                           "$line isa line, has name \"{}\";\n"
+                           "insert\n"
+                           "$route isa route, has identifier {};\n"
+                           "(operated-by: $line, operates: $route) isa operation;"
+                           ).format(data['lineName'], station_intervals["id"])
 
-            for station_intervals in routes["stationIntervals"]:
-                last_naptan_id = data['timetable']["departureStopId"]
-                last_time_to_arrival = 0
-                # print("----- id : {} -----".format(station_intervals["id"]))
-                yield ("match\n"
-                       "$service isa service, has name \"{}\";\n"
-                       "insert\n"
-                       "$route isa route, has identifier {};\n"
-                       "(operated-by: $service, operates: $route) isa operation;"
-                       ).format(data['lineName'], station_intervals["id"])
+                    for i, interval in enumerate(station_intervals['intervals']):
+                        origin_termination_flag = ""
+                        if i == 0:
+                            origin_termination_flag = ", has is-origin true"
+                        elif i == len(station_intervals['intervals']) - 1:
+                            origin_termination_flag = ", has is-termination true"
+                        yield (
+                            "match\n"
+                            "$a isa stop, has naptan-id \"{}\";\n"
+                            "$b isa stop, has naptan-id \"{}\";\n"
+                            "$r isa route, has identifier \"{}\";\n"
+                            "insert\n"
+                            "$rs(goes-from: $a, goes-to: $b, part-of: $r) isa route-section, has duration {}{};").format(
+                            last_naptan_id,
+                            interval["stopId"],
+                            station_intervals["id"],
+                            int(interval["timeToArrival"] - last_time_to_arrival),
+                            origin_termination_flag
+                        )
 
-                for i, interval in enumerate(station_intervals['intervals']):
-                    origin_termination_flag = ""
-                    if i == 0:
-                        origin_termination_flag = ", has is-origin true"
-                    elif i == len(station_intervals['intervals']) - 1:
-                        origin_termination_flag = ", has is-termination true"
-                    yield (
-                        "match\n"
-                        "$a isa stop, has naptan-id \"{}\";\n"
-                        "$b isa stop, has naptan-id \"{}\";\n"
-                        "$r isa route, has identifier \"{}\";\n"
-                        "insert\n"
-                        "$rs(goes-from: $a, goes-to: $b, part-of: $r) isa route-section, has duration {}{};").format(
-                        last_naptan_id,
-                        interval["stopId"],
-                        station_intervals["id"],
-                        int(interval["timeToArrival"] - last_time_to_arrival),
-                        origin_termination_flag
-                    )
-
-                    last_time_to_arrival = interval["timeToArrival"]
-                    last_naptan_id = interval["stopId"]
+                        last_time_to_arrival = interval["timeToArrival"]
+                        last_naptan_id = interval["stopId"]
 
 
-def make_queries(query_generator, keyspace, uri='http://localhost:4567', log_file="logs/graql_output_{}.txt".format(dt.datetime.now())):
+def make_queries(query_generator, timetables_dir_path, keyspace, uri='http://localhost:4567', log_file="logs/graql_output_{}.txt".format(dt.datetime.now())):
     client = grakn.Client(uri=uri, keyspace=keyspace)
 
     start_time = dt.datetime.now()
     # log_file = "logs/graql_output_{}.txt".format(dt.datetime.now())
     with open(log_file, "w") as graql_output:
-        for query in query_generator():
+        for query in query_generator(timetables_dir_path):
             print(query)
             print("---")
             graql_output.write(query)
@@ -106,4 +116,18 @@ def make_queries(query_generator, keyspace, uri='http://localhost:4567', log_fil
     print("Time taken: {}".format(time_taken))
 
 
-make_queries(import_query_generator, "tube_example")
+if __name__ == "__main__":
+
+    download_dir_name = "data/data-downloads"
+
+    routes_path = "{}/{}/routes/".format(os.path.dirname(__file__), download_dir_name)
+    timetables_dir_path = "{}/../{}/timetables/".format(os.path.dirname(__file__), download_dir_name)
+
+    go = True
+
+    if go:
+
+        make_queries(import_query_generator, timetables_dir_path, "tube_example_5")
+    else:
+        for query in import_query_generator(timetables_dir_path):
+            print(query)
