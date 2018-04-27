5e90502824a79bb92820f0c358ba14d51e0d9d0a
Unknown <james@grakn.ai>
2018-04-27 17:20:35 +0100

    Cleaning and refinement

--- transportation-example/src/migrations/all_routes_migration.py
+++ transportation-example/src/migrations/all_routes_migration.py
@@ -1,6 +1,7 @@
 import json
 import grakn
 import datetime as dt
+import os
 
 
 def routes_generator(routes):
@@ -81,29 +82,36 @@ def import_query_generator():
                 to_relate += ", comprises: $route-section-{}".format(i)
 
             to_relate += ") isa composition;\n"
-            yield to_match + to_insert + to_relate
+            yield to_match + to_insert + to_relate + "\n"
+
+if __name__ == "__main__":
+
+    download_dir_name = "data-downloads"
+    routes_path = "{}/{}/routes/".format(download_dir_name, os.path.dirname(__file__))
+    timetables_path = "{}/{}/timetables/".format(download_dir_name, os.path.dirname(__file__))
 
 
-client = grakn.Client(uri='http://localhost:4567', keyspace='transportation_example')
+    client = grakn.Client(uri='http://localhost:4567', keyspace='insert_bug_1')
 
-start_time = dt.datetime.now()
-log_file = "logs/graql_output_{}.txt".format(dt.datetime.now())
-with open(log_file, "w") as graql_output:
-    for i, query in enumerate(import_query_generator()):
-        print(query)
-        print("---")
-        graql_output.write(query)
-        # graql_output.write("---")
-        # Feed the queries one at a time
-        response = client.execute(query)
-        graql_output.write(str(response))
-        graql_output.write("\n{} insertions made \n ----- \n".format(len(response)))
-        if len(response) == 0:
-            raise RuntimeError("Tried to make an insertion, but no concepts could be inserted. Check entities in \""
-                               "match\" clause")
+    start_time = dt.datetime.now()
+    log_file = "logs/graql_output_{}.txt".format(dt.datetime.now())
+    with open(log_file, "w") as graql_output:
+        for i, query in enumerate(import_query_generator()):
+            print(query)
+            print("---")
+            graql_output.write(query)
+            # graql_output.write("---")
+            # Feed the queries one at a time
+            response = client.execute(query)
+            graql_output.write(str(response))
+            graql_output.write("\n{} insertions made \n ----- \n".format(len(response)))
+            if len(response) == 0:
+                raise RuntimeError("Tried to make an insertion, but no concepts could be inserted. Check entities in \""
+                                   "match\" clause")
 
-        # TODO how to do complex matches to get the variables of 2 different things without inefficiency
-        # TODO Northern Rail takes some time to complete
-end_time = dt.datetime.now()
-time_taken = end_time - start_time
-print("Time taken: {}".format(time_taken))
+            # TODO how to do complex matches to get the variables of 2 different things without inefficiency
+            # TODO Northern Rail takes some time to complete
+
+    end_time = dt.datetime.now()
+    time_taken = end_time - start_time
+    print("Time taken: {}".format(time_taken))
