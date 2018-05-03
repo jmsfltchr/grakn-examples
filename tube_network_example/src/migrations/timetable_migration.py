6a1b3b383168bb1b6ac12c296b0f3c568e3814ac
Unknown <james@grakn.ai>
2018-05-03 19:03:32 +0100

    Begins changing to new schema and allows server responses to be read

--- tube_network_example/src/migrations/timetable_migration.py
+++ tube_network_example/src/migrations/timetable_migration.py
@@ -2,65 +2,94 @@ import json
 import grakn
 import datetime as dt
 import os
-
 import tube_network_example.settings as settings
 from utils.assertions import check_response_length
 
 
-def id_generator():
-    i = 0
-    while 1:
-        yield i
-        i += 1
+def match(graql_body):
+    prefix = "match "
+    return prefix + graql_body
 
 
-def import_query_generator(timetables_dir_path):
+def match_get(graql_body):
+    suffix = " get;"
+    return match(graql_body) + suffix
+
+
+def insert(graql_body):
+    prefix = "insert "
+    return prefix + graql_body
+
+
+def match_insert(match_graql_body, insert_graql_body):
+    return match(match_graql_body) + "\n" + insert(insert_graql_body)
+
+
+def get_match_id(match_get_response):
+    return match_get_response[0]['id']
+
+
+def import_query_generator(perform_query, timetables_dir_path):
     """
     Builds the Grakl statements required to import the transportation data contained in all_routes.json
     :return:
     """
 
     timetable_paths = os.listdir(timetables_dir_path)
-    zone_names = set()
-    stop_ids = set()
-    line_names = set()
-    route_id_gen = id_generator()
+
+    station_query = "$s1 isa station, has naptan-id \"{}\";"
 
     for timetable_path in timetable_paths:
         with open(timetables_dir_path + "/" + timetable_path, 'r') as f:
             data = json.load(f)
-            if data['lineName'] not in line_names:
-                line_names.add(data['lineName'])
-                yield ("insert $line isa line, has name \"{}\";").format(data['lineName'])
 
-            for stop in data["stops"]:
-                if stop["id"] not in stop_ids:
-                    # Only proceed if we haven't already seen this stop before
-                    stop_ids.add(stop["id"])
-                    # TODO Check that the stop isn't duplicated, or skip this if instead we only do it once per tube line.
-                    # - Actually there willbe duplication doing it per tube, since stations belong to more than one line
+            tube_line_query = "$tl isa tube-line, has name \"{}\";".format(data['lineName'])
+            response = perform_query(match_get(tube_line_query))
+
+            if len(response) < 1:
+                # In this case we haven't already added this tube-line before
+                # We do it this way so that we have explicitly asked the database if the tube-line exists, and if not
+                # then we use the same query body but as an insert
+
+                response = perform_query(insert(tube_line_query))
+                check_response_length(response, min_length=1, max_length=1) # Exactly one concept should be inserted
+
+            for station in data["stops"]:
+
+                response = perform_query(match_get(station_query.format(station["id"])))
+
+                if len(response) < 1:
+                    # Only proceed if there is this station isn't already in the database
+
+                    # Check that the station isn't duplicated, or skip this if instead we only do it once per tube-line.
+                    # - Actually there will be duplication doing it per tube, since stations belong to more than one
+                    # tube-line
                     try:
-                        zone_name = stop["zone"]
+                        zone_name = station["zone"]
                     except KeyError:
                         # In the case that there is no zone information
                         zone_name = -1
 
-                    if zone_name not in zone_names:
-                        zone_names.add(zone_name)
-                        yield ("insert $zone isa zone, has name \"{}\";".format(zone_name))
+                    zone_query = "$z isa zone, has name \"{}\";\n".format(zone_name)
 
-                    yield ("match\n"
-                           "$zone isa zone, has name \"{}\";\n"
-                           "insert\n"
-                           "$stop isa stop, has naptan-id \"{}\", has name \"{}\", "
-                           "has lat {}, has lon {};\n"
-                           "(contains-stop: $stop, within: $zone) isa zoning;").format(
-                        zone_name,
-                        stop["id"],
-                        stop["name"],
-                        stop["lat"],
-                        stop["lon"]
-                )
+                    response = perform_query(match_get(zone_query))
+
+                    if len(response) < 1:
+                        # If the zone doesn't already exist then insert it
+                        response = perform_query(insert(zone_query))
+                        check_response_length(response, min_length=1, max_length=1) # Exactly one concept should be
+                        # inserted
+
+                    station_insert_query = (station_query.format(station["id"]) +
+                                            "$s1 has name \"{}\", "
+                                            "has lat {}, has lon {};\n"
+                                            "$z(contained-station: $s1)").format(station["id"],
+                                                                                 station["name"],
+                                                                                 station["lat"],
+                                                                                 station["lon"],
+                                                                                 )
+                    response = perform_query(match_insert(zone_query, station_insert_query))
+                    check_response_length(response, min_length=1, max_length=1)
 
             """
             Get the time between stops
@@ -68,27 +97,66 @@ def import_query_generator(timetables_dir_path):
             for routes in data['timetable']["routes"]:
 
                 for station_intervals in routes["stationIntervals"]:
+                    # This actually iterates over the routes, in TFL's infinite wisdom
+
                     last_naptan_id = data['timetable']["departureStopId"]
                     last_time_to_arrival = 0
-                    route_id = next(route_id_gen)
-                    yield ("match\n"
-                           "$line isa line, has name \"{}\";\n"
-                           "insert\n"
-                           "$route isa route, has identifier {};\n"
-                           "(operated-by: $line, operates: $route) isa operation;"
-                           ).format(data['lineName'], route_id)
+                    route_query = "$r(route-operator: $tl) isa route;"
+
+                    response = perform_query(match_insert(tube_line_query, route_query))
+                    check_response_length(response, min_length=1, max_length=1)
+                    route_id = response[0]['id']
+                    # TODO Here we need to execute the query in order to get the ID of the route inserted, since that's
+                    # the only way to uniquely identify it for the insertion of the route-sections below
 
                     for i, interval in enumerate(station_intervals['intervals']):
+
+                        # === TUNNELS ===
+                        # Now we need to insert a tunnel that can make the connection if there isn't one, or if there
+                        # is one then don't add one and instead use its ID
+                        station_pair_query = ("$s1 isa station, has naptan-id \"{}\";\n"
+                                              "$s2 isa station, has naptan-id \"{}\";\n"
+                                              ).format(last_naptan_id,
+                                                       interval["stopId"])
+
+                        tunnel_query = "$t(start: $s1, end: $s2) isa tunnel;"
+
+                        response = perform_query(match_get(station_pair_query + tunnel_query))
+
+                        if len(response) < 1:
+                            response = perform_query(match_insert(station_pair_query, tunnel_query))
+
+                        # Get the ID of either the pre-existing tunnel or the one just inserted
+                        tunnel_id = get_match_id(response)
+
+                        # === Connect Stations to Routes ===
                         origin_termination_flag = ""
                         if i == 0:
-                            origin_termination_flag = ", has is-origin true"
+                            # In this case we're at the first route-section of the route, starting at the first station
+                            role_played = "origin"
+
                         elif i == len(station_intervals['intervals']) - 1:
-                            origin_termination_flag = ", has is-termination true"
-                        yield (
+                            # In this case we're at the last route-section of the route, ending at the last station
+                            # TODO This doesn't work as it doesn't get the last stop, it gets the one before last
+                            # TODO instead this needs to happen as an EXTRA step on the last iteration. In that iteration there are 2 stops to add.
+                            # TODO Or this can be changed around to add 2 the first iteration (1 before the loop)
+                            role_played = "destination"
+                        else:
+                            role_played = "stop"
+
+                        match_query = station_query.format(last_naptan_id)
+                        insert_query = "$r({}: $s1) isa route id {}".format(role_played, route_id)
+
+                        response = perform_query(match_insert(match_query, insert_query))
+                        check_response_length(response, min_length=1, max_length=1)
+
+                        if i == len(station_intervals['intervals']) - 1:
+
+                        response = perform_query((
                             "match\n"
-                            "$a isa stop, has naptan-id \"{}\";\n"
-                            "$b isa stop, has naptan-id \"{}\";\n"
-                            "$r isa route, has identifier {};\n"
+                            "$a isa station, has naptan-id \"{}\";\n"
+                            "$b isa station, has naptan-id \"{}\";\n"
+                            "$r isa route, id {};\n"
                             "insert\n"
                             "$rs(goes-from: $a, goes-to: $b, part-of: $r) isa route-section, has duration {}{};").format(
                             last_naptan_id,
@@ -96,43 +164,44 @@ def import_query_generator(timetables_dir_path):
                             route_id,
                             int(interval["timeToArrival"] - last_time_to_arrival),
                             origin_termination_flag
-                        )
+                        ))
 
                         last_time_to_arrival = interval["timeToArrival"]
                         last_naptan_id = interval["stopId"]
 
 
-def make_queries(query_generator, timetables_dir_path, keyspace, uri=settings.uri, log_file="logs/graql_output_{}.txt".format(dt.datetime.now())):
+def make_queries(timetables_dir_path, keyspace, uri=settings.uri,
+                 log_file="logs/graql_output_{}.txt".format(dt.datetime.now()), send_queries=True):
     client = grakn.Client(uri=uri, keyspace=keyspace)
 
     start_time = dt.datetime.now()
-    # log_file = "logs/graql_output_{}.txt".format(dt.datetime.now())
+
     with open(log_file, "w") as graql_output:
-        for query in query_generator(timetables_dir_path):
-            print(query)
+
+        def query_function(graql_string):
+            print(graql_string)
             print("---")
-            graql_output.write(query)
-            # Feed the queries one at a time
-            response = client.execute(query)
-            graql_output.write("\n--response:\n" + str(response))
-            graql_output.write("\n{} insertions made \n ----- \n".format(len(response)))
-            check_response_length(response, min_length=1, max_length=None)
+            graql_output.write(graql_string)
+            if send_queries:
+                # Send the graql query to the server
+                response = client.execute(graql_string)
+                graql_output.write("\n--response:\n" + str(response))
+                graql_output.write("\n{} insertions made \n ----- \n".format(len(response)))
+                return response
+            else:
+                return ["<DUMMY_RESPONSE>"]
 
-            # TODO how to do complex matches to get the variables of 2 different things without inefficiency
-            # TODO Northern Rail takes some time to complete
+        import_query_generator(query_function, timetables_dir_path)
 
-    end_time = dt.datetime.now()
-    time_taken = end_time - start_time
-    print("Time taken: {}".format(time_taken))
+
+        end_time = dt.datetime.now()
+        time_taken = end_time - start_time
+        time_taken_string = "----------\nTime taken: {}".format(time_taken)
+        graql_output.write(time_taken_string)
+        print(time_taken_string)
 
 
 if __name__ == "__main__":
 
-    go = True
-
-    if go:
-
-        make_queries(import_query_generator, settings.timetables_path, settings.keyspace)
-    else:
-        for query in import_query_generator(settings.timetables_path):
-            print(query)
+    go = False
+    make_queries(settings.timetables_path, settings.keyspace, send_queries=go)
