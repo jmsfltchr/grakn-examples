d986d504a809280c391236fdbc1d20a5ec4f0cb4
Unknown <james@grakn.ai>
2018-05-09 17:54:24 +0100

    Explores finding stations along a shortest path via routes

--- tube_network_example/src/journey_planner.py
+++ tube_network_example/src/journey_planner.py
@@ -20,12 +20,22 @@ if __name__ == "__main__":
     # TO:
     # b_name = "Edgware Road (Circle Line) Underground Station"
     b_name = "Queensbury Underground Station"
+    # b_name = "Upminster Underground Station"  # Tries to return 1048576 paths, which is 2^20, implying 1 path of 20 steps length
+
+    FEWEST_STOPS = 0
+    FEWEST_ROUTES = 1
+    method = FEWEST_STOPS
 
     a_id = get_match_id(perform_query(match_get("$s1 isa station, has name \"{}\";".format(a_name))), "s1")
     b_id = get_match_id(perform_query(match_get("$s1 isa station, has name \"{}\";".format(b_name))), "s1")
-    # compute_query = "compute path from {}, to {}, in [station, tunnel];".format(a_id, b_id)  # Fewest stops
-    compute_query = "compute path from {}, to {}, in [station, route];".format(a_id, b_id)  # Fewest changes
+
+    if method == FEWEST_STOPS:
+        compute_query = "compute path from {}, to {}, in [station, tunnel];".format(a_id, b_id)  # Fewest stops
+    elif method == FEWEST_ROUTES:
+        compute_query = "compute path from {}, to {}, in [station, route];".format(a_id, b_id)  # Fewest changes
+    print("Finding shortest paths...")
     shortest_paths = perform_query(compute_query)
+    print("...done")
     # print(shortest_paths)
 
     # The response contains the different permutations for each path through stations. We are mainly interested in
@@ -54,3 +64,19 @@ if __name__ == "__main__":
 
     # TODO find the duration of the valid paths found
     # TODO first the paths need to be filtered somehow to show that they are connected
+
+    if method == FEWEST_ROUTES:
+        # In the case where we have done shortest path {in station, route} rather than {in station, tunnel}
+        # TODO unfortunately at present we can't access the explanation for the reasoning from Python, so we can't find
+        # TODO the stations that these routes pass through
+        for path in shortest_paths:
+            for i in range(1, len(path), 2):
+                # We should have the pattern station, route, station, route, station...
+                s1_id = path[i - 1]['id']
+                r_id = path[i]['id']
+                s2_id = path[i + 1]['id']
+                match_query = ("match\n"
+                               "$s1 id {}; $s2 id {}; $r id {}; "
+                               "(start: $s1, finish: $s2, leg-route: $r) isa journey-leg; get;").format(s1_id, s2_id, r_id)
+                response = perform_query(match_query)
+                # explanation = #TODO No explanation given in Python
