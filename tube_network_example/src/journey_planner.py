0c9f1f55a3609eac16b7795ebc0060b841e4d3f3
Unknown <james@grakn.ai>
2018-05-22 17:56:53 +0100

    Removes unwanted file, adds Apache V2 licences

--- tube_network_example/src/journey_planner.py
+++ tube_network_example/src/journey_planner.py
@@ -1,3 +1,17 @@
+# Copyright 2018 Grakn Labs Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import grakn
 import tube_network_example.settings as settings
 from utils.utils import check_response_length, match_get, insert, match_insert, get_match_id
